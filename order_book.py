14229b87cdf9a6cd709e6991973d9beeb69dee8e
xiaohan1mcit <77126016+xiaohan1mcit@users.noreply.github.com>
2021-07-16 06:33:44 -0400

    Create order_book.py

---
+++ order_book.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+
+from models import Base, Order
+engine = create_engine('sqlite:///orders.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+def process_order(order):
+    #Your code here
+    print('haha')
+    pass
