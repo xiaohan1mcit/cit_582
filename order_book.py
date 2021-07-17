29679a9ebc86a78c5b7486903186a9694e4b78a8
xiaohan1mcit <77126016+xiaohan1mcit@users.noreply.github.com>
2021-07-17 04:41:32 -0400

    Create order_book.py

---
+++ order_book.py
@@ -0,0 +1,31 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+
+from models import Base, Order
+
+engine = create_engine('sqlite:///orders.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+def process_order(order):
+    # Your code here
+    # type of order is <class 'dict'>
+
+    print(order)
+
+    # Insert the order into the database
+    # type of order_obj is <class 'models.Order'>
+    order_obj = Order(sender_pk=order['sender_pk'],
+                      receiver_pk=order['receiver_pk'],
+                      buy_currency=order['buy_currency'],
+                      sell_currency=order['sell_currency'],
+                      buy_amount=order['buy_amount'],
+                      sell_amount=order['sell_amount'])
+    session.add(order_obj)
+    session.commit()
+
+    # get the order you just inserted from the DB
+    current_order = session.query(Order).order_by(Order.id.desc()).first()
