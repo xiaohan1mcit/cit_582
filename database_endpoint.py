3a42d0667a81a9558870a0abe6ad6265f2bee7fd
xiaohan1mcit <77126016+xiaohan1mcit@users.noreply.github.com>
2021-07-25 06:43:14 -0400

    Update database_endpoint.py

--- database_endpoint.py
+++ database_endpoint.py
@@ -175,11 +175,10 @@ def order_book():
         'data': order_dict_list
     }    
     
+    print("order book length: ")
     print(len(order_dict_list))
     print(order_dict_list[-2])
     print(order_dict_list[-1])
-    print(order_dict_list[-1]['buy_amount'])
-    print(type(order_dict_list[-1]['buy_amount']))
 
     shutdown_session()
     return jsonify(result)
