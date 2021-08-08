6fa92011037230f52fd84546dd15e08da6f3a5bc
xiaohan1mcit <77126016+xiaohan1mcit@users.noreply.github.com>
2021-08-08 00:00:39 -0400

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -240,7 +240,7 @@ def trade():
                               signature=sig)            
             g.session.add(order_obj)
             
-            fill_order()
+#             fill_order()
             shutdown_session()
             return jsonify(result)
         
