898f7b0cd8ba44377525096d1ba3cb0e17989a4a
xiaohan1mcit <77126016+xiaohan1mcit@users.noreply.github.com>
2021-08-08 00:47:19 -0400

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -274,8 +274,10 @@ def order_book():
     
     print("order book length: ")
     print(len(order_dict_list))
-    print(order_dict_list[-2])
-    print(order_dict_list[-1])
+    print(order_dict_list[-2]['creator_id'])
+    print(order_dict_list[-2]['child'])
+    print(order_dict_list[-1]['creator_id'])
+    print(order_dict_list[-2]['child'])
 
     shutdown_session()
     return jsonify(result)
