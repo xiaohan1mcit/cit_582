180c643e4ec497a4b7d24f9956d4ddf0534180df
xiaohan1mcit <77126016+xiaohan1mcit@users.noreply.github.com>
2021-08-14 03:43:17 -0400

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -332,8 +332,21 @@ def execute_txes(txes):
     #       1. Send tokens on the Algorand and eth testnets, appropriately
     #          We've provided the send_tokens_algo and send_tokens_eth skeleton methods in send_tokens.py
     #       2. Add all transactions to the TX table
+    for tx_dict in algo_txes:
+        tx = TX(platform = tx_dict['platform'],
+                 receiver_pk = tx_dict['receiver_pk'],
+                 order_id = tx_dict['order_id'])
+        g.session.add(tx)
+        g.session.commit()
+    
+    for tx_dict in eth_txes:
+        tx = TX(platform = tx_dict['platform'],
+                 receiver_pk = tx_dict['receiver_pk'],
+                 order_id = tx_dict['order_id'])
+        g.session.add(tx)
+        g.session.commit()
 
-    pass
+#     pass
 
 
 
