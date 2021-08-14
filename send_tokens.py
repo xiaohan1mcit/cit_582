0b92d503f5b2939159717af8d0e7a6e52255b12c
xiaohan1mcit <77126016+xiaohan1mcit@users.noreply.github.com>
2021-08-14 15:04:09 -0400

    Update send_tokens.py

--- send_tokens.py
+++ send_tokens.py
@@ -39,7 +39,7 @@ def connect_to_algo(connection_type=''):
 def send_tokens_algo( acl, sender_sk, txes):
     params = acl.suggested_params
     
-    print("hehehehehehehehehehe")
+   
     
     # TODO: You might want to adjust the first/last valid rounds in the suggested_params
     #       See guide for details
@@ -60,6 +60,7 @@ def send_tokens_algo( acl, sender_sk, txes):
         # TODO: Sign the transaction
         signed_tx = "Replace me with a SignedTransaction object"
         
+        print("hehehehehehehehehehe")
         
         
 #         try:
