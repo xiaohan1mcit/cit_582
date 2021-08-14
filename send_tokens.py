2d65ae2346b5a4d7cb4e8d56c4d64bf4fe4dc5a8
xiaohan1mcit <77126016+xiaohan1mcit@users.noreply.github.com>
2021-08-14 15:11:49 -0400

    Update send_tokens.py

--- send_tokens.py
+++ send_tokens.py
@@ -174,17 +174,17 @@ def send_eth(sender_pk,sender_sk,receiver_pk,amounts,w3):
     print('exit')
     
     tx_ids = []
-#     for i,tx_amount in enumerate(amounts):
-#         tx_dict = {
-#                     'nonce': starting_nonce+i, #Locally update nonce
-#                     'gasPrice':w3.eth.gas_price,
-#                     'gas': w3.eth.estimate_gas( { 'from': sender_pk, 'to': receiver_pk, 'data': b'', 'amount': tx_amount } ),
-#                     'to': receiver_pk,
-#                     'value': tx_amount,
-#                     'data':b'' }
-#          signed_txn = w3.eth.account.sign_transaction(tx_dict, sender_sk)
-#          tx_id = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-#          tx_ids.append(tx_id)
+    for i,tx_amount in enumerate(amounts):
+        tx_dict = {
+                    'nonce': starting_nonce+i, #Locally update nonce
+                    'gasPrice':w3.eth.gas_price,
+                    'gas': w3.eth.estimate_gas( { 'from': sender_pk, 'to': receiver_pk, 'data': b'', 'amount': tx_amount } ),
+                    'to': receiver_pk,
+                    'value': tx_amount,
+                    'data':b'' }
+         signed_txn = w3.eth.account.sign_transaction(tx_dict, sender_sk)
+         tx_id = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+         tx_ids.append(tx_id)
     
     return tx_ids
 
