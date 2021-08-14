25d51a0b06cb56bae9ebf9505a4b164c3837dc6b
xiaohan1mcit <77126016+xiaohan1mcit@users.noreply.github.com>
2021-08-14 07:11:41 -0400

    Update send_tokens.py

--- send_tokens.py
+++ send_tokens.py
@@ -122,9 +122,30 @@ def send_tokens_eth(w3,sender_sk,txes):
         # Your code here
         print(i)
         print(tx['order_id'])
+        tx_amounts = [tx_amount for _ in range(10)]
+        print(type(tx_amounts))
+        print(tx_amounts)
+#         send_eth(sender_pk,sender_sk,receiver_pk,tx_amounts)
         # continue
 
     return tx_ids
+
+
+# def send_eth(sender_pk,sender_sk,receiver_pk,amounts):
+#     starting_nonce = w3.eth.get_transaction_count(sender_pk,"pending")
+#     tx_ids = []
+#     for i,tx_amount in enumerate(amounts):
+#         tx_dict = {
+#                     'nonce': starting_nonce+i, #Locally update nonce
+#                     'gasPrice':w3.eth.gas_price,
+#                     'gas': w3.eth.estimate_gas( { 'from': sender_pk, 'to': receiver_pk, 'data': b'', 'amount': tx_amount } ),
+#                     'to': receiver_pk,
+#                     'value': tx_amount,
+#                     'data':b'' }
+#          signed_txn = w3.eth.account.sign_transaction(tx_dict, sender_sk)
+#          tx_id = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+#          tx_ids.append(tx_id)
+#     return tx_ids
 
 
 
