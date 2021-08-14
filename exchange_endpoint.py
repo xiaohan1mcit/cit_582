dc7ce47139fa65ddfde3d3f045beda69356b4672
xiaohan1mcit <77126016+xiaohan1mcit@users.noreply.github.com>
2021-08-14 06:58:04 -0400

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -342,8 +342,9 @@ def execute_txes(txes):
     print_tx_list(algo_txes)
     print_tx_list(eth_txes)
     
-    eth_print()
-    print("\n\n\nwoozie\n\n\n")
+    eth_print()    
+    w3 = Web3()
+    send_tokens_eth(w3, eth_sk, txes):
 
 
     # TODO: 
