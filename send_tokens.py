ba83866265afb93f7bdd5832866502b099e08148
xiaohan1mcit <77126016+xiaohan1mcit@users.noreply.github.com>
2021-08-15 00:08:21 -0400

    Update send_tokens.py

--- send_tokens.py
+++ send_tokens.py
@@ -187,10 +187,11 @@ def send_eth(sender_pk,sender_sk,receiver_pk,amounts,w3):
                     'data':b'' }
         signed_txn = w3.eth.account.sign_transaction(tx_dict, sender_sk)
         tx_id = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-        print(type(tx_id))
+#         print(type(tx_id))
         tx_ids.append(tx_id)
     
     print('exit')
+    print(len(tx_ids))
     return tx_ids
 
 
