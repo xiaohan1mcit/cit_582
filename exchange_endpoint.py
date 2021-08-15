cb4ae439a10ff3a7d6d5a7ab400e57caf280b42c
xiaohan1mcit <77126016+xiaohan1mcit@users.noreply.github.com>
2021-08-15 01:04:48 -0400

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -371,10 +371,11 @@ def execute_txes(txes):
         g.session.add(tx)
         g.session.commit()
     
-    for tx_dict in eth_txes:
+    for i, tx_dict in enumerate(eth_txes):
         tx = TX(platform = tx_dict['platform'],
                  receiver_pk = tx_dict['receiver_pk'],
-                 order_id = tx_dict['order_id'])
+                 order_id = tx_dict['order_id'], 
+                 tx_id = eth_tx_ids[i])
         g.session.add(tx)
         g.session.commit()
         
