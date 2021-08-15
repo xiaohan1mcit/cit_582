b0848b49dd77115045fbaadeed6615fccaaaa2bd
xiaohan1mcit <77126016+xiaohan1mcit@users.noreply.github.com>
2021-08-15 00:33:19 -0400

    Update send_tokens.py

--- send_tokens.py
+++ send_tokens.py
@@ -158,7 +158,7 @@ def send_tokens_eth(w3,sender_sk,txes):
         tx_amounts = [tx_amount for _ in range(10)]
         receiver_pk = tx['receiver_pk']  
         tx_ids_inner = send_eth(sender_pk,sender_sk,receiver_pk,tx_amounts,w3)
-        tx_ids.append(tx_ids_inner[0])
+#         tx_ids.append(tx_ids_inner[0])
         print('success')
         # continue
 
