f121eabc98c7fab31a469ad2df1dcdcddc348ff9
xiaohan1mcit <77126016+xiaohan1mcit@users.noreply.github.com>
2021-08-15 22:05:18 -0400

    Update send_tokens.py

--- send_tokens.py
+++ send_tokens.py
@@ -69,10 +69,13 @@ def send_tokens_algo( acl, sender_sk, txes):
         tx_fee = params.min_fee
         send_amount = 1
         
-        
+        receiver = tx['receiver_pk']
+        amount = tx['amount']
+        print(receiver)
+        print(amount)
     
         # "Replace me with a transaction object"
-        unsigned_tx = transaction.PaymentTxn(sender_pk, tx_fee, first_valid_round, last_valid_round, gen_hash, tx['receiver_pk'], tx['amount'],
+        unsigned_tx = transaction.PaymentTxn(sender_pk, tx_fee, first_valid_round, last_valid_round, gen_hash, receiver, amount,
                                  flat_fee=True)
         print("reach")
 
